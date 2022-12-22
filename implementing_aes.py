3b5f2b2179e005f964c8bbab9ce97ec47b96ce45
NetGo+ Dev <36708000+rickxy@users.noreply.github.com>
2022-12-22 20:50:07 +0300

    Update implementing_aes.py

--- implementing_aes.py
+++ implementing_aes.py
@@ -173,7 +173,6 @@ def xtimes_0b(b):
     # 0x0b = 11 = b1011 = ((x*2)*2+x)*2+x
     return xtime(xtime(xtime(b)) ^ b) ^ b
 
-
 def xtimes_0d(b):
     # 0x0d = 13 = b1101 = ((x*2+x)*2)*2+x
     return xtime(xtime(xtime(b) ^ b)) ^ b
